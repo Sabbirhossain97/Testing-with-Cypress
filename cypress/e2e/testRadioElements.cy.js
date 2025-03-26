8e41afdaa986779516fbadbbf77c5dd75b0e2204
Sabbirhossain97 <sabbirhossainbd199@gmail.com>
2025-03-27 00:41:00 +0600

    testing ui elements

---
+++ cypress/e2e/testRadioElements.cy.js
@@ -0,0 +1,37 @@
+describe("test radio and checkbox elements", () => {
+    it("test radio buttons", () => {
+        cy.visit("http://127.0.0.1:5500/")
+
+        //visibilty of radio buttons
+        cy.get("input[value='male']").should('be.visible')
+        cy.get("input[value='female']").should('be.visible')
+        cy.get("input[value='others']").should('be.visible')
+
+        //selecting radio buttons
+        cy.get("input[value='male']").check().should('be.checked')
+        cy.get("input[value='female']").should('not.be.checked')
+
+        cy.get("input[value='female']").check().should('be.checked')
+        cy.get("input[value='male']").should('not.be.checked')
+    })
+
+    it("test checkboxes ", () => {
+        cy.visit("http://127.0.0.1:5500/")
+
+        //visibilty of checkbox
+        cy.get("input[value='option1']").should('be.visible')
+        cy.get("input[value='option2']").should('be.visible')
+        cy.get("input[value='option3']").should('be.visible')
+        cy.get("input[value='option4']").should('be.visible')
+
+        // //selecting checkbox
+        cy.get("input[value='option1']").check().should('be.checked')
+
+        //unselecting checkbox
+        cy.get("input[value='option1']").uncheck().should('not.be.checked')
+
+        // //selecting all checkbox at a time
+        cy.get("input.check-input[type='checkbox']").last().check()
+
+    })
+})
